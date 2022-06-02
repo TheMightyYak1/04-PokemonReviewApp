086cdba9b66c33ad583d7ca4492dc739bca30a2b
Brendan Baker <bren.r.baker@gmail.com>
2022-06-03 08:58:27 +1000

    initial commit

---
+++ PokemonReviewApp/Interfaces/IPokemonRepository.cs
@@ -0,0 +1,4 @@
+public interface IPokemonRepository
+{
+    ICollection<Pokemon> GetPokemons();
+}
---
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -0,0 +1,14 @@
+public class PokemonRepository : IPokemonRepository
+{
+    private readonly DataContext _context;
+
+    public PokemonRepository(DataContext context)
+    {
+        _context = context;
+    }
+
+    public ICollection<Pokemon> GetPokemons()
+    {
+        return _context.Pokemon.OrderBy(p => p.Id).ToList();
+    }
+}
